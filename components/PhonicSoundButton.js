bc76f680e68b89c28dfa0d2ede9d9e9f9544a600
SaidTheCoder <71198810+SaidTheCoder@users.noreply.github.com>
2021-01-14 19:34:31 -0500

    Add files via upload

---
+++ components/PhonicSoundButton.js
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Audio } from 'expo-av';
+
+export default class PhonicSoundButton extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      pressedButtonIndex:'',
+    }
+  }
+  playSound = async soundChunk => {
+    console.log(soundChunk);
+    var soundLink =
+      'https://whitehatjrcontent.s3.ap-south-1.amazonaws.com/phones/' +
+      soundChunk +
+      '.mp3';
+    await Audio.Sound.createAsync(
+      {
+        uri: soundLink,
+      },
+      { shouldPlay: true }
+    );
+  };
+  render() {
+    return (
+      <TouchableOpacity
+        style={
+          this.props.buttonIndex===this.state.pressedButtonIndex
+          ?[styles.chunkButton,{backgroundColor:'blue'}]
+          :[styles.chunkButton,{backgroundColor:'red'}]
+        }
+        onPress={() => {
+          this.setState({pressedButtonIndex:this.props.buttonIndex})
+          this.playSound(this.props.soundChunk);
+        }}>
+        <Text style={
+          this.props.buttonIndex===this.state.pressedButtonIndex
+          ?[styles.displayText,{backgroundColor:'red'}]
+          :[styles.displayText,{backgroundColor:'blue'}]
+        }>{this.props.wordChunk}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  displayText: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: 'white'
+  },
+  chunkButton:{
+    width: '60%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderRadius: 10,
+    margin: 5,
+    backgroundColor: 'red'
+  }
+});
